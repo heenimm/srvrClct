1c94df867f54ada6897905a20e09843c854f36ec
asmag <halimlka@MacBook-Pro-As.local>
2025-05-09 08:55:27 +0300

    add jwt

--- auth/jwt.go
+++ auth/jwt.go
@@ -1,1 +1,27 @@
 package auth
+
+import (
+	"github.com/golang-jwt/jwt/v5"
+	"time"
+)
+
+var jwtKey = []byte("your-secret-key")
+
+func GenerateJWT(userID int) (string, error) {
+	claims := jwt.MapClaims{
+		"user_id": userID,
+		"exp":     time.Now().Add(24 * time.Hour).Unix(),
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(jwtKey)
+}
+
+func ParseJWT(tokenStr string) (int, error) {
+	token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
+		return jwtKey, nil
+	})
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		return int(claims["user_id"].(float64)), nil
+	}
+	return 0, err
+}
