7b253eb4386a0b49280cd4f6abb1fa5548851f85
asmag <halimlka@MacBook-Pro-As.local>
2025-05-12 01:10:44 +0300

    add correct

--- storage/db.go
+++ storage/db.go
@@ -6,8 +6,6 @@ import (
 	"os"
 )
 
-//var DB *sqlx.DB
-
 func InitDB(dbPath string) (*sqlx.DB, error) {
 	db, err := sqlx.Connect("sqlite3", dbPath)
 	if err != nil {
@@ -32,37 +30,3 @@ func ApplySchema(db *sqlx.DB, schemaPath string) error {
 	}
 	return nil
 }
-
-//package storage
-//
-//import (
-//	"github.com/jmoiron/sqlx"
-//	_ "github.com/mattn/go-sqlite3"
-//	"log"
-//	"os"
-//)
-//
-//var DB *sqlx.DB
-//
-//func InitDB(dbPath string) (*sqlx.DB, error) {
-//	var err error
-//	DB, err = sqlx.Connect("sqlite3", dbPath)
-//	if err != nil {
-//		log.Printf("error connecting to database: %v", err)
-//		return nil, err
-//	}
-//
-//	schema, err := os.ReadFile("../db/schema.sql")
-//	if err != nil {
-//		log.Printf("failed to read schema file: %v", err)
-//		return nil, err
-//	}
-//
-//	_, err = DB.Exec(string(schema))
-//	if err != nil {
-//		log.Printf("failed to execute schema: %v", err)
-//		return nil, err
-//	}
-//
-//	return DB, nil
-//}
